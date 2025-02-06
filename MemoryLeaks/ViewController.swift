b273978d78698138dad07cca726b3b510b28d7a4
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-06 16:23:28 +0500

    Here in this code the memory leak gets occurs

--- MemoryLeaks/ViewController.swift
+++ MemoryLeaks/ViewController.swift
@@ -23,11 +23,29 @@ class ViewController: UIViewController {
     
 }
 
+class myView: UIView {
+    
+    let vc: UIViewController
+    
+    init(vc: UIViewController) {
+        self.vc = vc
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
 class SecondVC: UIViewController {
+    
+    var myView: UIView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        // myView = myView(vc: self)
     }
     
 }
